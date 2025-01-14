43d70845bd8659de27456586030b40d5c587deba
Aditya Anugrah <134223672+AdiityaAnugrah@users.noreply.github.com>
2025-01-14 14:10:25 +0700

    update : Seesion

--- app/Config/Session.php
+++ app/Config/Session.php
@@ -21,7 +21,7 @@ class Session extends BaseConfig
      *
      * @var class-string<BaseHandler>
      */
-    public string $driver = FileHandler::class;
+    public string $driver = FileHandler::class; 
 
     /**
      * --------------------------------------------------------------------------
@@ -40,7 +40,7 @@ class Session extends BaseConfig
      * The number of SECONDS you want the session to last.
      * Setting to 0 (zero) means expire when the browser is closed.
      */
-    public int $expiration = 7200;
+    public int $expiration = 28800;
 
     /**
      * --------------------------------------------------------------------------
@@ -57,7 +57,7 @@ class Session extends BaseConfig
      *
      * IMPORTANT: You are REQUIRED to set a valid save path!
      */
-    public string $savePath = WRITEPATH . 'session';
+    public string $savePath = WRITEPATH . 'session'; 
 
     /**
      * --------------------------------------------------------------------------
@@ -78,7 +78,7 @@ class Session extends BaseConfig
      *
      * How many seconds between CI regenerating the session ID.
      */
-    public int $timeToUpdate = 300;
+    public int $timeToUpdate = 3000; 
 
     /**
      * --------------------------------------------------------------------------
@@ -89,7 +89,7 @@ class Session extends BaseConfig
      * when auto-regenerating the session ID. When set to FALSE, the data
      * will be later deleted by the garbage collector.
      */
-    public bool $regenerateDestroy = false;
+    public bool $regenerateDestroy = false; 
 
     /**
      * --------------------------------------------------------------------------
--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> <?= $title ?> | TITANIUM</title>
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="<?= base_url('../dist/assets/extensions/flatpickr/flatpickr.min.css'); ?>">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/dripicons@1.0.0/webfont/css/dripicons.min.css">
     <link rel="shortcut icon" href="<?= base_url('../dist/assets/compiled/svg/favicon.svg'); ?>" type="image/x-icon">
